b307f02680c86a8266c1f007a3fbbdd578d95838
Dylan Fisher <dyfisher12@gmail.com>
2025-12-08 00:11:54 -0600

    adjusted printed text

--- src/image_generator_final.py
+++ src/image_generator_final.py
@@ -3,10 +3,10 @@ import random
 
 
 def gather_inputs():
-    background_img = input("Select your background: \nDeath City \nHyrule Field \nThe Moon \nMinecraft Cave")
-    character_A = input("Select your first character: \nLink \nDeath the Kid \nPapa Emeritus IV \n Perry the Platypus")
-    character_B = input("Select your second character: \nKing Bob \nGeralt of Rivia \nFoxy \nBatman")
-    character_pet = input("Select a pet for your characters: \nGarfield \nNavi \nBolt \nDimple")
+    background_img = input("Select your background: \nA) Death City \nB) Hyrule Field \nC) The Moon \nD) Minecraft Cave")
+    character_A = input("Select your first character: \nA) Link \nB) Death the Kid \nC) Papa Emeritus IV \nD) Perry the Platypus")
+    character_B = input("Select your second character: \nA) King Bob \nB) Geralt of Rivia \nC) Foxy \nD) Batman")
+    character_pet = input("Select a pet for your characters: \nA) Garfield \nB) Navi \nC) Bolt \nD) Dimple")
     quote_select = input("Would you like to display a random generated quote? \nYes/No")
 
     
