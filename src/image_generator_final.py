e38b66fec26d7dc1c2c435fb64eebe81103d86b3
Dylan Fisher <dyfisher12@gmail.com>
2025-12-08 20:54:07 -0600

    added images to folders

--- src/image_generator_final.py
+++ src/image_generator_final.py
@@ -1,12 +1,18 @@
 from PIL import Image, ImageFile
 import random
 
+def main():
 
-def gather_inputs():
+    
     background_img = input("Select your background: \nA) Death City \nB) Hyrule Field \nC) The Moon \nD) Minecraft Cave \nE) Random")
     character_A = input("Select your first character: \nA) Link \nB) Death the Kid \nC) Papa Emeritus II \nD) Perry the Platypus \nE) Random")
     character_B = input("Select your second character: \nA) King Bob \nB) Geralt of Rivia \nC) Foxy \nD) Batman \nE) Random")
     character_pet = input("Select a pet for your characters: \nA) Garfield \nB) Navi \nC) Bolt \nD) Dimple \nE) Random")
     quote_select = input("Would you like to display a randomly generated quote? \nYes/No")
 
+    def create_image():
+        if background_img == "e":
+            
+        
     
+    create_image()
