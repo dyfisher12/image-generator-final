52c2a2ba5b42e1a2e139fb17fc78f6aee8d6d170
Dylan Fisher <dyfisher12@gmail.com>
2025-12-08 18:43:08 -0600

    added bg images

--- src/image_generator_final.py
+++ src/image_generator_final.py
@@ -7,6 +7,6 @@ def gather_inputs():
     character_A = input("Select your first character: \nA) Link \nB) Death the Kid \nC) Papa Emeritus IV \nD) Perry the Platypus \nE) Random")
     character_B = input("Select your second character: \nA) King Bob \nB) Geralt of Rivia \nC) Foxy \nD) Batman \nE) Random")
     character_pet = input("Select a pet for your characters: \nA) Garfield \nB) Navi \nC) Bolt \nD) Dimple \nE) Random")
-    quote_select = input("Would you like to display a random generated quote? \nYes/No")
+    quote_select = input("Would you like to display a randomly generated quote? \nYes/No")
 
     
